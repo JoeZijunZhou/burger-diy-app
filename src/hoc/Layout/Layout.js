a7ed9a0d9f6762283341900a688a8aab9bbaf114
Zijun Zhou <zzj940219@gmail.com>
2018-06-16 12:34:22 -0700

    basic version done! --structure improvement + bug fix

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
-import Aux from '../../hoc/Auxi'
+import Aux from '../Auxi/Auxi'
 import classes from './Layout.css'
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 //change layout to class ->stateful
 class Layout extends Component {
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
 
     sideDrawerClosedHandler = () => {

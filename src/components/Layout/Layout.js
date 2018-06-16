d056bb137e583fecbf2370b36e41c931e21dffe6
Zijun Zhou <zzj940219@gmail.com>
2018-06-16 10:59:05 -0700

    navigation--items+logo toolbar+sidedrawer(pc+mobile version)

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Aux from '../../hoc/Auxi'
 import classes from './Layout.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className={classes.Content}>
-        {props.children}
-    </main>
+        <Toolbar/>
+        <SideDrawer/>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
     </Aux>
 );
 
